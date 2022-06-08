93c566d53426d86f7980d9d77fe116013a2b8617
sherzodAlxibekov <you@example.com>
2022-06-09 01:09:21 +0500

    new lessons editor

--- Javascript-lessons-master/Lessons-Javascript/lesson-29-dars. Amaliyot. DOM elemental bilan/js/script.js
+++ Javascript-lessons-master/Lessons-Javascript/lesson-29-dars. Amaliyot. DOM elemental bilan/js/script.js
@@ -1,9 +1,9 @@
 'use strict'
 
 const adv = document.querySelectorAll('.promo__adv img'),
-  wrapper = document.querySelector('.promo__bg'),
-  genre = wrapper.querySelector('.promo__genre'),
-  seriesList = document.querySelector('.promo__interactive-list')
+    wrapper = document.querySelector('.promo__bg'),
+    genre = wrapper.querySelector('.promo__genre'),
+    seriesList = document.querySelector('.promo__interactive-list')
 
 const seriesDB = {
   series: [
